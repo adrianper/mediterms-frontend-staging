34bed4a351d6656623c57e6fbae01a9aa37fc961
ALAN CORRALEJO <>
2023-01-19 20:26:53 -0600

    feat: maquetacion RecoverPassword

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -6,6 +6,7 @@ import { Button, Grid, TextField, Text } from 'components'
 import { routes } from 'routing/routes'
 
 import './login.scss'
+import RecoverPassword from 'pages/RecoverPassword/RecoverPassword'
 import { login/*, reset*/ } from 'redux/reducers/auth/authSlice'
 // import { toast } from 'react-toastify'
 
@@ -14,7 +15,7 @@ import { login/*, reset*/ } from 'redux/reducers/auth/authSlice'
 
 const Login = () => {
     const [formData, setFormData] = useState({ email: '', password: '' })
-
+    const [recoverPassword, setRecoverPassword] = useState(false)
     // const { auth } = useSelector(store => store)
     // const { /*user, */authenticated } = auth
 
@@ -50,33 +51,36 @@ const Login = () => {
     // }, [authenticated, navigate])
 
     return (
-        <Grid className="login" itemsX="center" gap="4.28em" padding="4.28em 0em 0em 0em">
+        <Grid className="login" itemsX="center" gap="4.28em" padding="4.28em 0.42em 0em 0.42em">
             <img src="https://inteligeneresources.s3.us-east-2.amazonaws.com/Imagenes/mediterms-logo.png" />
+            {recoverPassword ?
+                <RecoverPassword setRecoverPassword={setRecoverPassword} />
+            :
+                <form onSubmit={handleSumbit}>
+                    <Grid w100 padding="1.72em 1.1em" className="login__form" gap="1.3em" maxWidth="22em">
+                        <Text size="5" align="center" bold>Inicia sesión</Text>
+                        <TextField label="Correo electrónico"
+                            type="email"
+                            value={formData.email}
+                            onChange={v => handleChange(v, 'email')}
+                        />
+                        <TextField label="Contraseña"
+                            type="password"
+                            value={formData.password}
+                            onChange={v => handleChange(v, 'password')}
+                        />
+                        <Text size="2" color="error">El correo electrónico o la contraseña son incorrectos, intenta de nuevo.</Text>
+                        <Button type="submit" selfCenter>Ingresar</Button>
 
-            <form onSubmit={handleSumbit}>
-                <Grid w100 padding="1.72em 1.1em" className="login__form" gap="1.3em" maxWidth="22em">
-                    <Text size="5" align="center" bold>Inicia sesión</Text>
-                    <TextField label="Correo electrónico"
-                        type="email"
-                        value={formData.email}
-                        onChange={v => handleChange(v, 'email')}
-                    />
-                    <TextField label="Contraseña"
-                        type="password"
-                        value={formData.password}
-                        onChange={v => handleChange(v, 'password')}
-                    />
-                    <Text size="2" color="error">El correo electrónico o la contraseña son incorrectos, intenta de nuevo.</Text>
-                    <Button type="submit" selfCenter>Ingresar</Button>
-
-                    <Grid gap="2em" margin="1em 0em 0em 0em">
-                        <PageLink to={routes.signup.path} >
-                            <Text medium align="center" color="first">Abrir una cuenta</Text>
-                        </PageLink>
-                        <Text medium align="center" color="second">Olvidé mi contraseña</Text>
+                        <Grid gap="2em" margin="1em 0em 0em 0em">
+                            <PageLink to={routes.signup.path} >
+                                <Text medium align="center" color="first">Abrir una cuenta</Text>
+                            </PageLink>
+                            <Text onClick={() => {setRecoverPassword(true)}} medium align="center" color="second">Olvidé mi contraseña</Text>
+                        </Grid>
                     </Grid>
-                </Grid>
-            </form>
+                </form>
+            }
             {/*<Button type="submit" onClick={() => dispatch(reset())}>Reset auth</Button>*/}
         </Grid>
     )
---
+++ src/pages/RecoverPassword/RecoverPassword.js
@@ -0,0 +1,34 @@
+import React, { useCallback,/* useEffect,*/ useState } from 'react'
+import { Button, Grid, TextField, Text } from 'components'
+import './recover_password.scss'
+
+const RecoverPassword = (props) =>{
+    const { setRecoverPassword } = props
+    const [recoveredPassword, setRecoveredPassword] = useState(false)
+
+    return(
+        <Grid>
+            <Grid w100 padding="1.72em 1.1em" className="recover_password"  maxWidth="22em">
+                {!recoveredPassword ?
+                <Grid gap="1.3em">
+                    <Text bold align="center" size="5">Recuperar tu contraseña</Text>
+                    <TextField label="Correo electrónico"
+                        type="email"
+                        
+                        // onChange={v => handleChange(v, 'recover_password')}
+                    />
+                    <Text medium align="center">Enviaremos información a este correo electrónico para recuperar tu contraseña.</Text>
+                    <Button selfCenter onClick={() => {setRecoveredPassword(true)}}>Recuperar</Button>
+                </Grid>
+                :
+                <Grid padding="3.78em 0em" gap="1.71em">
+                    <Text medium align="center">Se ha enviado una contraseña provisional al correo electrónico.</Text>
+                    <Button selfCenter onClick={() => {setRecoverPassword(false)}}>Entendido</Button>
+                </Grid>
+                }
+            </Grid>
+        </Grid>
+    )
+}
+
+export default RecoverPassword
