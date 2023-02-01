f1e46001d665b53db0507ead84d58fccda6aa028
OmarMurillo <omar.murillo@inteligene.com>
2023-02-01 14:38:05 -0700

    fix: Get the account info using a request

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -7,13 +7,15 @@ import { routes } from 'routing/routes'
 import './account.scss'
 import axios from 'axios'
 
+const DEFAULT_PROFILE_PHOTO = "https://magiei-resources.s3.us-east-2.amazonaws.com/Icons/icon-user-edit.svg"
 
 const Account = () => {
     const [topicWithTotal, setTopicsWithTotal] = useState([])
     const [error, setError] = useState([])
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
-    const DEFAULT_PROFILE_PHOTO = "https://magiei-resources.s3.us-east-2.amazonaws.com/Icons/icon-user-edit.svg"
+    const [photoUrl, setPhotoUrl] = useState(DEFAULT_PROFILE_PHOTO)
+    
     const [selectedImage, setSelectedImage] = useState('');
 
     useEffect(() => {
@@ -24,12 +26,26 @@ const Account = () => {
                 'Authorization': `Bearer ${localStorage.getItem('token')}`
             }
         }).then(res => {
-            setTopicsWithTotal(res.data)
-            let data = JSON.parse(localStorage.getItem('user'))
-            setName(data.name)
-            setEmail(data.email)   
+            setTopicsWithTotal(res.data) 
         }).catch(err => {
-            console.log(err.response.statusText)
+            setError(err.response.statusText)
+            
+        })
+    },[])
+
+    useEffect(() => {
+        axios.get('/user/account', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json;charset=UTF-8',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        }).then(res => {
+            const {data} = res
+            setName(data.user.name)
+            setEmail(data.user.email)   
+            setPhotoUrl(data.user.photoUrl || DEFAULT_PROFILE_PHOTO)
+        }).catch(err => {
             setError(err.response.statusText)
             
         })
@@ -58,15 +74,16 @@ const Account = () => {
         }
         axios(options)
         .then(response => {
-            console.log(response.data.photoUrl)
+            setPhotoUrl(response.data.photoUrl || DEFAULT_PROFILE_PHOTO)
         })
         .catch(err =>{
             console.log(err)
         })
     }, [])
 
-    let userObject =  JSON.parse(localStorage.getItem('user'))
-    const photoUrl = userObject.photoUrl === '' ? <img src={DEFAULT_PROFILE_PHOTO} className="user_info__default"/> : <img src={userObject.photoUrl} className="account__user_photo" />
+
+    const imageClassName = photoUrl === DEFAULT_PROFILE_PHOTO ? "user_info__default" : "account__user_photo"
+
     return (
         <Grid className="account" itemsX="center" gap="0.7em" padding="1.14em 0.42em">
             <Grid w100 gap="1.71em" itemsX="center" padding="1.71em 4.57em" className="account__user_info">
@@ -79,7 +96,7 @@ const Account = () => {
                             changeUserPhoto(event);
                         }}
                     />
-                    {photoUrl}
+                    <img src={photoUrl} className={imageClassName}/>
                     <Text medium>{name}</Text>
                     <Text medium>{email}</Text>
                 </Grid>
