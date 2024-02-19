0d32a696c858182ea8bd362ce4984fcf427cc2bb
adrianper <adrian.perez@inteligene.com>
2024-02-18 23:03:58 -0700

    feat: Verify account status before allow answer topics

--- src/routing/RequireAuth.js
+++ src/routing/RequireAuth.js
@@ -1,26 +1,48 @@
 import { useSelector } from "react-redux"
-import { Navigate, Outlet, useLocation } from "react-router-dom"
+import { Navigate, Outlet, useLocation, useNavigate } from "react-router-dom"
 import { noRedirectPaths, routes } from "./routes"
-import { NoVerifiedAccount, Payment } from "pages"
+import { NoVerifiedAccount/*, Payment*/ } from "pages"
+import { useEffect } from "react"
+import axios from "axios"
 
 const RequireAuth = () => {
-    const { authenticated, verified, accountStatus } = useSelector(store => store.auth)
+    const { authenticated, verified/*, accountStatus */ } = useSelector(store => store.auth)
     const location = useLocation()
     const locationState = {}
 
+    const navigate = useNavigate()
+
     if (!noRedirectPaths.includes(location.pathname)) locationState.from = location
-    
-    if(location.pathname === "/noVerifiedAccount" && authenticated && location.pathname !== "/verifiedAccount") return <NoVerifiedAccount />
-    
+
+    const validateSession = async () => {
+        try {
+            const response = await axios.get('session/')
+            if (response.data.accountStatus === 'MDT-AS-US_PR_0000') {
+                localStorage.setItem("md_ac_u_s", response.data.accountStatus)
+                navigate('/payment')
+            }
+        } catch (error) {
+            console.error('VALIDATE_SESSION_ERROR', error)
+        }
+    }
+
+    useEffect(() => {
+        if (authenticated)
+            validateSession()
+        // eslint-disable-next-line
+    }, [])
+
+    if (location.pathname === "/noVerifiedAccount" && authenticated && location.pathname !== "/verifiedAccount") return <NoVerifiedAccount />
+
     // if(location.pathname === "/payment" && authenticated) return <Payment />
 
     // console.log("authenticated", authenticated, "accountStatus", accountStatus )
     // if(authenticated && accountStatus === 'MDT-AS-US_PR_0000') return <Navigate replace to={'/payment'} />
 
-    if(authenticated && verified){
-        return <Outlet /> 
+    if (authenticated && verified) {
+        return <Outlet />
     }
-    if(authenticated && !verified){
+    if (authenticated && !verified) {
         return <Navigate replace to={routes.noVerifiedAccount.path} />
     }
 
